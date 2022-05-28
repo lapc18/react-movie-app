43441f330f22f509741b14755726af7d16989d8e
lapc18 <lapc18@outlook.com>
2022-05-28 17:36:21 -0400

    feat: add drawer molecule component

---
+++ src/components/molecules/drawer/Drawer.jsx
@@ -0,0 +1,35 @@
+import {
+  Box,
+  Drawer,
+  List,
+} from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import { nanoid } from "@reduxjs/toolkit";
+import { ListItemLink } from "../../atoms/list-item-link/ListItemLink";
+
+export const AppDrawer = ({ isOpen = false, onClose }) => {
+  const items = (
+    <List>
+      {drawerItems().map((item, index) => (
+        <ListItemLink key={index} {...item} />
+      ))}
+    </List>
+  );
+
+  return (
+    <Drawer anchor="left" open={isOpen} onClose={() => onClose && onClose()}>
+      <Box sx={{ width: 250 }} role="presentation">
+        {items}
+      </Box>
+    </Drawer>
+  );
+};
+
+const drawerItems = () => [
+  {
+    id: nanoid(),
+    text: "Home",
+    to: "/",
+    icon: <HomeIcon />,
+  },
+];
