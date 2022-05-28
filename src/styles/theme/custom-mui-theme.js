6b4cf8e32014069a0fb501a200d79bdadaf19acc
lapc18 <lapc18@outlook.com>
2022-05-28 17:31:13 -0400

    feat: set base colors

--- src/styles/theme/custom-mui-theme.js
+++ src/styles/theme/custom-mui-theme.js
@@ -4,23 +4,23 @@ import { createTheme } from "@mui/material";
 export const customTheme = createTheme({
     palette: {
         primary: {
-          main: '#ffbbb5',
+          main: '#0e2541',
         },
         secondary: {
-          main: '#e53935',
+          main: '#215697',
         },
     },
     components: {
       MuiDrawer: {
         styleOverrides: {
           paper: {
-            backgroundColor: "#ffbbb5",
+            backgroundColor: "#317ad4",
             color: "#ffffff",
           }
         }
       },
       MuiTypography: {
-        defaultProps: {color: "#000"}
+        defaultProps: {color: "#fff"}
       }
     },
 });
