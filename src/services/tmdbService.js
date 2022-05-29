858fbebee8255df3dd9e5c81ab65f982bb1699bf
lapc18 <lapc18@outlook.com>
2022-05-29 15:51:31 -0400

    feat: get films that are in theaters(tmdbService)

--- src/services/tmdbService.js
+++ src/services/tmdbService.js
@@ -1,3 +1,13 @@
 import httpCommon from "../common/http-common";
 
-export const getAll = () => httpCommon.get("/popular?language=en-US&page1");
+/**
+ * 
+ * @param {*} page Current page to be requested. 
+ * @param {*} language Current language to be requested. 
+ * @returns Get a list of movies in theatres. 
+ * @description This is a release type query that looks for all movies that have a release type of 2 or 3 within the specified date range.
+ */
+export const getAll = async ({page = 1, language = 'en-US'}) => {
+    const response = await httpCommon.get(`/now_playing?language=${language}&page=${page}`);
+    return response.data['results'];
+}
