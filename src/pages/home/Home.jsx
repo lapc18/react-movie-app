552d54aebaef80d25b80384544fa9835a7ebe262
lapc18 <lapc18@outlook.com>
2022-05-29 16:04:16 -0400

    feat: show initial movies grid from Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { MovieGrid } from "../../components/organism/movies-grid/MovieGrid";
+import { fetchMovies } from "../../store/slices/moviesSlice";
 
 export const Home = () => {
-    return (<></>);
-}
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movie.movies);
+
+  useEffect(() => {
+    dispatch(fetchMovies({ page: 1, language: "us-US" }));
+  }, [dispatch]);
+
+  return (
+    <>
+      <MovieGrid movies={movies} />
+    </>
+  );
+};
